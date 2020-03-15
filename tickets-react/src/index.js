2bfb61e722b7e0d383454e44222071a9dc076f9b
Pramoch Chulapong <pramoch.chulapong@scb.co.th>
2020-03-15 13:53:03 +0700

    Add Redux Devtools

--- tickets-react/src/index.js
+++ tickets-react/src/index.js
@@ -5,11 +5,12 @@ import App from './components/App';
 
 // Redux
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
